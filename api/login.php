f415100f339497ab3337c94195da5750533f04da
Kusindra Aji Rabbany <me@benspace.xyz>
2023-09-06 19:07:18 +0700

    feat: add login register system

--- api/login.php
+++ api/login.php
@@ -1,3 +1,30 @@
 <?php
+if ($_POST) {
+  include "../database.php";
+  // collect value of input field
+  $email = $_POST['email'];
+  $password = $_POST['password'];
+    
+  $result = mysqli_query($conn, "SELECT * FROM `user` WHERE email='".$email."'")->fetch_row();
+  if ($result){
+    session_start();
+    $pass= $result[3];
 
+    $ciphering = "AES-128-CTR";
+    $iv_length = openssl_cipher_iv_length($ciphering);
+    $options = 0;
+    $encryption_iv = '1234567891011121';
+    $encryption_key = "ResToo0R4An";
+    $decryption = openssl_decrypt($pass, $ciphering,
+    $encryption_key, $options, $encryption_iv);
+    if ($decryption == $password){
+      $_SESSION["user"]["email"]=$email;
+      $_SESSION["user"]["role"]=$result[4];
+      header("Location: ../");
+    } else header("Location: ../login");
+  } else {
+    header("Location: ../login");
+  }
+  die();
+}
 ?>
---
+++ api/register.php
@@ -0,0 +1,20 @@
+<?php
+if ($_POST) {
+  include "../database.php";
+  // collect value of input field
+  $email = $_POST['email'];
+  $name = $_POST['name'];
+  $password = $_POST['password'];
+  $ciphering = "AES-128-CTR";
+  $iv_length = openssl_cipher_iv_length($ciphering);
+  $options = 0;
+  $encryption_iv = '1234567891011121';
+  $encryption_key = "ResToo0R4An";
+  $encryption = openssl_encrypt($password, $ciphering,
+              $encryption_key, $options, $encryption_iv);
+    
+  $result = mysqli_query($conn, "INSERT INTO `user` ( `email`, `name`, `password` ) values('".$email."', '".$name."', '".$encryption."') ");
+  header("Location: ../login");
+  die();
+}
+?>
